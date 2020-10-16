873cad752a4ab30e7f019bae12895147db3aa90b
Frank Sustaita <fsequence@gmail.com>
2020-10-16 13:04:25 -0700

    10) Removing Duplicates from a Sequence while Maintaining Order

--- Code/Data_Structures_and_Algorithms_Part_2.py
+++ Code/Data_Structures_and_Algorithms_Part_2.py
@@ -7,7 +7,8 @@
 # 7) Keeping Dictionaries in Order, line 105
 # 8) Calculating with Dictionaries, line 162
 # 9) Finding Commonalities in Two Dictionaries, line 287
-# 10)
+# 10) Removing Duplicates from a Sequence while Maintaining Order, line 357
+# 11)
 
 
 # -------------------------------------------------------------------------
@@ -352,3 +353,97 @@ c = {key: a[key] for key in a.keys() - {'z', 'w'}}
 # to be unique. This alone makes certain set operations of questionable
 # utility. However, if you must perform such calculations, they can be
 # accomplished by simply converting the values to a set first.
+
+
+# 10) Removing Duplicates from a Sequence while Maintaining Order
+
+
+# You want to eliminate the duplicate values in a sequence, but preserve
+# the order of the remaining items.
+
+# If the values in the sequence are hashable, the problem can be easily
+# solved using a set and a generator.
+
+
+# For example:
+
+
+def dedupe(items):
+    seen = set()
+    for item in items:
+        if item not in seen:
+            yield item
+            seen.add(item)
+
+
+# Here is an example of how to use your function:
+
+
+a = [1, 5, 2, 1, 9, 1, 5, 10]
+
+list(dedupe(a))
+# [1, 5, 2, 9, 10]
+
+
+# This only works if the items in the sequence are hashable. If you are
+# trying to eliminate duplicates in a sequence of unhashable types (such
+# as dicts), you can make a slight change to this recipe, as follows:
+
+
+def dedupe(items, ke=None):
+    seen = set()
+    for item in items:
+        val = item if key is None else key(item)
+        if val not in seen:
+            yield item
+            seen.add(val)
+
+
+# Here, the purpose of the key argument is to specify a function that
+# converts sequence items into a hashable type for the purposes of duplicate
+# detection. Here's how it works:
+
+
+a = [{'x':1, 'y':2}, {'x':1, 'y':3}, {'x':1, 'y':2}, {'x':2, 'y':4}]
+
+list(dedupe(a, key=lambda d: (d['x'],d['y'])))
+# [{'x':1, 'y':2}, {'x':1, 'y':3}, {'x':2, 'y':4}]
+
+list(dedupe(a, key=lambda d: d['x']))
+# [{'x':1, 'y':2}, {'x':2, 'y':4}]
+
+
+# This latter solution also works nicely if you want to eliminate duplicates
+# based on the value of a single field or attribute or a larger data structure.
+
+# If all you want to do is eliminate duplicates, it is often easy enough
+# to make a set.
+
+
+# For example:
+
+
+a
+# [1, 5, 2, 1, 9, 1, 5, 10]
+
+set(a)
+{1, 2, 10, 5, 9}
+
+
+# However, this approach doesn't preserve any kind of ordering. So, the
+# resulting data will be scrambled afterward. The solution shown avoids
+# this.
+
+# The use of a generator function in this recipe reflects the fact that you
+#  might want the function to be extremely general purpose - not necessarily
+# tied directly to list processing. For example, if you want to read a file,
+# eliminating duplicate lines, you could simple do this:
+
+
+with open(somefile, 'r') as f:
+    for line in dedupe(f):
+        ...
+
+
+# The specification of a key function mimics similiar functionality in
+# built-in functions such as sorted(), min() and max().
