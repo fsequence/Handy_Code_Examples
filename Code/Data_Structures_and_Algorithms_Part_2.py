24fd007c79f20908bd74696576d3e32185f51046
Frank Sustaita <fsequence@gmail.com>
2020-10-18 22:19:43 -0700

    11) Naming a slice

--- Code/Data_Structures_and_Algorithms_Part_2.py
+++ Code/Data_Structures_and_Algorithms_Part_2.py
@@ -8,7 +8,7 @@
 # 8) Calculating with Dictionaries, line 162
 # 9) Finding Commonalities in Two Dictionaries, line 287
 # 10) Removing Duplicates from a Sequence while Maintaining Order, line 357
-# 11)
+# 11) Naming a slice, line 452
 
 
 # -------------------------------------------------------------------------
@@ -447,3 +447,103 @@ with open(somefile, 'r') as f:
 
 # The specification of a key function mimics similiar functionality in
 # built-in functions such as sorted(), min() and max().
+
+
+# 11) Naming a Slice
+
+
+# Your program has become an unreadable mess of hardcoded slice indices and
+# you want to clean it up.
+
+# Suppose you have some code that is pulling specific data fields out of a
+# record string with fixed fields (e.g., from a flat file or similar
+# format):
+
+
+# ######## 2908217348907230489020982734897239847987234987239487'
+record = '....................100        .........513.25     ............'
+cost = int(record[20:32]) * float(record[40:48])
+
+
+# Instead of doing that, why not name the slices like this?
+
+
+SHARES = slice(20,32)
+PRICE = slice(40,48)
+
+cost = int(record[SHARES]) * float(record[PRICE])
+
+
+# In the latter version, you avoid having a lot of mysterious hardcoded
+# indices, and what you're doing becomes much clearer.
+
+# As a general rule, writing code with a lot of hardcoded index values
+# leads to a readability and maintenance mess. For example, if you come
+# back to the code a year later, you'll look at it and wonder what you were
+# thinking when you wrote it. The solution shown is simply a way of more
+# clearly stating what your code is actually doing.
+
+# In general, the built-in slice() creates a slice object that can be used
+# anywhere a slice is allowed.
+
+
+# For example:
+
+
+items = [0, 1, 2, 3, 4, 5, 6]
+a = slice(2, 4)
+
+items[2:4]
+# [2,3]
+
+items[a]
+# [2,3]
+
+items[a] = [10, 11]
+# items = [0, 1, 10, 11, 4, 5, 6]
+
+del items[a]
+
+items
+# [0, 1, 4, 5, 6]
+
+
+# If you have a slice instance s, you can get more information about it by
+# looking at its s.start, s.stop and s.step attributes, respecfully.
+
+
+# For example:
+
+
+a = slice(5, 50, 2)
+
+a.start
+# 5
+
+a.stop
+# 50
+
+a.step
+# 2
+
+
+# In addition, you can map a slice onto a sequence of a specific size by
+# using its indices(size) method. This returns a tuple (start, stop, step)
+# where all values have been suitably limited to fit within bounds (as to
+# avoid IndexError exceptions when indexing).
+
+
+# For example:
+
+
+s = 'HelloWorld'
+
+a.indices(len(s))
+# (5, 10, 2)
+
+for i in range(*a.indices(len(s))):
+    print(s[i])
+
+# W
+# r
+# d
