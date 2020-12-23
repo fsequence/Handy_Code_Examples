4f496bc7491764c7edc9764aaf582dffd22d6d91
Frank Sustaita <fsequence@gmail.com>
2020-12-22 23:47:55 -0800

    19) Transforming and Reducing Data at the Same Time

--- Code/Data_Structures_and_Algorithms_Part_4.py
+++ Code/Data_Structures_and_Algorithms_Part_4.py
@@ -5,7 +5,7 @@
 
 # 17) Extracting a Subset of a Dictionary, line 16
 # 18) Mapping Names to Sequence Elements, line 73
-# 19)
+# 19) Transforming and Reducing Data at the Same Time, line 237
 # 20)
 # 21)
 
@@ -232,3 +232,88 @@ dict_to_stock(b)
 # efficient data structure where you will be changing various instance
 # attributes, using namedtuple is not your best choice. Instead, consider
 # defining a class using __slots__ instead.
+
+
+# 19) Transforming and Reducing Data at the Same Time
+
+
+# You need to execute a reduction function (e.g. sum(), min(), max()), but
+# first need to transform or filter the data.
+
+# A very elegant way to combine a data reduction and a transformation is to
+# use a generator-expression argument. For example, if you want to calculate
+# the sum of squares, do the following:
+
+
+nums = [1, 2, 3, 4, 5]
+s = sum(x * x for x in nums)
+
+
+# Here are a very other examples:
+
+
+# Determine if any .py files exist in a directory
+
+import os
+files = os.listdir('dirname')
+if any(name.endswith('.py') for name in files):
+    print('There be python!')
+else:
+    print('Sorry, no python.')
+
+
+# Output a tuple as CSV
+
+s = ('ACME', 50, 123.45)
+print(','.join(str(x) for x in s))
+
+
+# Data reduction across fields of a data structure
+
+portfolio = [
+    {'name':'GOOG', 'shares': 50},
+    {'name':'YHOO', 'shares': 75},
+    {'name':'AOL', 'shares': 20},
+    {'name':'SCOX', 'shares': 65},
+]
+
+min_shares = min(s['shares'] for s in portfolio)
+
+
+# The solution shows a subtle syntactic aspect of generator expressions
+# when supplied as the single argument to a function (i.e. you don't need
+# repeated parentheses).
+
+
+# For example, these statements are the same:
+
+
+s = sum((x * x for x in nums))      # Pass generator-expr as argument
+s = sum(x * x for x in nums)        # More elegant syntax
+
+
+# Using a generator argument is often a more efficient and elegant approach
+# than first creating a temporary list. For example, if you didn't use a
+# generator expression, you might consider this alternative implementation:
+
+nums = [1, 2, 3, 4, 5]
+s = sum([x * x for x in nums])
+
+
+# This works, but it introduces an extra step and creates an extra list.
+# For such a small list, it might not matter, but if nums was huge, you
+# would end up creating a large temporary data structure to only be used
+# once and discarded. The generator solution transforms the data
+# iteratively and is therefore much more memory-efficient.
+
+# Certain reduction functions such as min() and max() accept a key argument
+# that might be useful in situations where you might be inclined to use a
+# generator. For example, in the portfolio example, you might consider this
+# alternative:
+
+
+# Original: Returns 20
+min = min(s['shares'] for s in portfolio)
+
+# Alternative: Returns {'name': 'AOL', 'shares': 20}
+min_shares = min(portfolio, key=lambda s: s['shares'])
