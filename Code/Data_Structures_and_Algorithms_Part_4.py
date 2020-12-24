c525c2d399b0b67be4f2f06ff7d23df2ccdda3e2
Frank Sustaita <fsequence@gmail.com>
2020-12-23 23:10:52 -0800

    20) Combining Multiple Mappings into a Single Mapping

--- Code/Data_Structures_and_Algorithms_Part_4.py
+++ Code/Data_Structures_and_Algorithms_Part_4.py
@@ -6,8 +6,7 @@
 # 17) Extracting a Subset of a Dictionary, line 16
 # 18) Mapping Names to Sequence Elements, line 73
 # 19) Transforming and Reducing Data at the Same Time, line 237
-# 20)
-# 21)
+# 20) Combining Multiple Mappings into a Single Mapping, line 321
 
 
 # -------------------------------------------------------------------------
@@ -249,7 +248,7 @@ nums = [1, 2, 3, 4, 5]
 s = sum(x * x for x in nums)
 
 
-# Here are a very other examples:
+# Here are a few other examples:
 
 
 # Determine if any .py files exist in a directory
@@ -317,3 +316,171 @@ min = min(s['shares'] for s in portfolio)
 
 # Alternative: Returns {'name': 'AOL', 'shares': 20}
 min_shares = min(portfolio, key=lambda s: s['shares'])
+
+
+# 20) Combining Multiple Mappings into a Single Mapping
+
+
+# You have multiple dictionaries or mappings that you want to logically
+# combine into a single mapping to perform certain operations, such as
+# looking up values or checking for the existence of keys.
+
+# Suppose you have two dictionaries:
+
+
+a = {'x': 1, 'z': 3}
+b = {'y': 2, 'z': 4}
+
+
+# Now suppose you want to perform lookups where you have to check both
+# dictionaries (e.g. first checking in a and then in b if not found). An
+# easy way to do this is to use the ChainMap class from the collections
+# module.
+
+
+# For example:
+
+
+from collections import ChainMap
+c = ChainMap(a, b)
+print(c['x'])       # Outputs 1 (from a)
+print(c['y'])       # Outputs 2 (from b)
+print(c['z'])       # Outputs 3 (from a)
+
+
+# A ChainMap takes multiple mappings and makes them logically appear as
+# one. However, the mappings are not literally merged together. Instead, a
+# ChainMap simply keeps a list of the underlying mappings and redefines
+# common dictionary operations to scan the list. Most operations will work.
+
+
+# For example:
+
+
+len(c)
+# 3
+
+list(c.keys())
+['x', 'y', 'z']
+
+list(c.values())
+[1, 2, 3]
+
+
+# If there are duplicate keys, the values from the first mapping get used.
+# Thus, the entry c['z'] in the example would always refer to the value in
+# dictionary a, not the value in dictionary b.
+
+# Operations that mutate the mapping always affect the first mapping
+# listed.
+
+
+# For example:
+
+
+c['z'] = 10
+c['w'] = 40
+del c['x']
+
+a
+# {'w': 40, 'z': 10}
+
+del c['y']
+# Traceback (most recent call last):
+# ...
+# KeyError: "Key not found in the first mapping: 'y'"
+
+
+# A ChainMap is particularly useful when working with scoped values such
+# as variables in a programming language (i.e., globals, locals, etc.). In
+# fact, there are methods that make this easy:
+
+
+values = ChainMap()
+values['x'] = 1
+
+# Add a new mapping
+values = values.new_child()
+values['x'] = 2
+
+# Add a new mapping
+values = values.new_child()
+values['x'] = 3
+
+values
+# ChainMap({'x': 3}, {'x': 2}, {'x': 1})
+
+values['x']
+# 3
+
+# Discard last mapping
+values = values.parents
+
+values['x']
+# 2
+
+# Discard last mapping
+values = values.parents
+
+values['x']
+# 1
+
+values
+# ChainMap({'x': 1})
+
+
+# As an alternative to ChainMap, you might consider merging dictionaries
+# together using the update() method.
+
+
+# For example:
+
+
+a = {'x': 1, 'z': 3}
+b = {'y': 2, 'z': 4}
+merged = dict(b)
+merged.update(a)
+
+merged['x']
+# 1
+
+merged['y']
+# 2
+
+merged['z']
+# 3
+
+
+# This works, but it requires you to make a completely separate dictionary
+# object (or destructively alter one of the existing dictionaries). Also,
+# if any of the original dictionaries mutate, the changes don't get
+# reflected in the merged dictionary.
+
+
+# For example:
+
+
+a['x'] = 13
+
+merged['x']
+# 1
+
+
+# A ChainMap uses the original dictionaries, so it doesn't have this
+# behavior.
+
+
+# For example:
+
+
+a = {'x': 1, 'z': 3}
+b = {'y': 2, 'z': 4}
+merged = ChainMap(a, b)
+
+merged['x']
+# 1
+
+a['x'] = 42
+
+merged['x']     # Notice change to merged dicts
+# 42
