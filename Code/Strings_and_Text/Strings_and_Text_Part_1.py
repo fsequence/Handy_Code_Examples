a5958b8d6d8798e3cbbd28bdf1d88a4ab8e52890
Frank Sustaita <fsequence@gmail.com>
2020-12-26 23:06:15 -0800

    2) Matching Text at the Start or End of a String

--- Code/Strings_and_Text/Strings_and_Text_Part_1.py
+++ Code/Strings_and_Text/Strings_and_Text_Part_1.py
@@ -4,7 +4,7 @@
 
 
 # 1) Splittings Strings on Any of Multiple Delimiters, line 15
-# 2)
+# 2) Matching Text at the Start or End of a String, line 87
 # 3)
 # 4)
 
@@ -82,3 +82,124 @@ delimiters
 
 re.split(r'(?:,|;|\s)\s*', line)
 # ['asdf', 'fjdk', 'afed', 'fjek', 'asdf', 'foo']
+
+
+# 2) Matching Text at the Start or End of a String
+
+
+# You need to check the start or end of a string for specific text
+# patterns, such as filename extensions, URL schemes, and so on.
+
+# A simple way to check the beginning or end of a string is to use the
+# str.startswith() or str.endswith() methods.
+
+
+# For example
+
+
+filename = 'spam.txt'
+
+filename.endswith('.txt')
+# True
+
+filename.startswith('file:')
+# False
+
+url = 'http://www.python.org'
+
+url.startswith('http:')
+# True
+
+
+# If you need to check against multiple choices, simply provide a tuple of
+# possibilities to startswith() or endswith():
+
+
+import os
+filenames = os.listdir('.')
+
+filenames
+# ['Makefile', 'foo.c', 'bar.py', 'spam.c', 'spam.h']
+
+[name for name in filenames if name.endswith(('.c', '.h'))]
+# ['foo.c', 'spam.c', 'spam.h' ]
+
+# any(name.endswith('.py') for name in filenames)
+# True
+
+
+# Here is another example:
+
+
+from urllib.request import urlopen
+
+def read_data(name):
+    if name.startswith(('http:', 'https:', 'ftp:')):
+        return urlopen(name).read()
+    else:
+        with open(name) as f:
+            return f.read()
+
+
+# Oddly, this is one part of Python where a tuple is actually required as
+# input. If you happen to have the choices specified in a list or set, just
+# make sure you convert them using tuple() first.
+
+
+# For example:
+
+
+choices = ['http:', 'ftp:']
+url = 'http://www.python.org'
+
+url.startswith(choices)
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# TypeError: startswith first arg must be str or a tuple of str, not list
+
+url.startswith(tuple(choices))
+# True
+
+
+# The startswith() and endswith() methods provide a very convenient way to
+# perform basic prefix and suffix checking. Similar operations can be
+# performed with slices, but are far less elegant.
+
+
+# For example:
+
+
+filename = 'spam.txt'
+
+filename[-4:] == '.txt'
+# True
+
+url = 'http://www.python.org'
+
+url[:5] == 'http:' or url[:6] == 'https:' or url[:4] == 'ftp:'
+# True
+
+
+# You might also be inclined to use regular expressions as an alternative.
+
+
+# For example:
+
+
+import re
+url = 'http://www.python.org'
+
+re.match('http:|https:|ftp:', url)
+# <_sre.SRE_Match object at 0x101253098>
+
+
+# This works, but is often overkill for simple matching. Using this example
+# is simpler and runs faster.
+
+# Last, but not least, the startswith() and endswith() methods look nice
+# when combined with other operations, such as common data reductions. For
+# example, this statement that checks a directory for the presence of
+# certain kinds of files:
+
+if any(name.endswith(('.c', '.h')) for name in listdir(dirname)):
+    ...
