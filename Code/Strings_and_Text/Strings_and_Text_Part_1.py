8d74fc3f4ffead756f3b31a4317eddb8e695b195
Frank Sustaita <fsequence@gmail.com>
2020-12-31 23:59:37 -0800

    4) Matching and Searching for Text Patterns

--- Code/Strings_and_Text/Strings_and_Text_Part_1.py
+++ Code/Strings_and_Text/Strings_and_Text_Part_1.py
@@ -6,7 +6,7 @@
 # 1) Splittings Strings on Any of Multiple Delimiters, line 15
 # 2) Matching Text at the Start or End of a String, line 87
 # 3) Matching Strings using Shell Wildcard Patterns, line 208
-# 4)
+# 4) Matching and Searching for Text Patterns, line 299
 
 
 # -------------------------------------------------------------------------
@@ -205,7 +205,7 @@ if any(name.endswith(('.c', '.h')) for name in listdir(dirname)):
     ...
 
 
-3) Matching Strings using Shell Wildcard Patterns
+# 3) Matching Strings using Shell Wildcard Patterns
 
 
 # You want to match text using the same wildcard patterns as are commonly
@@ -226,12 +226,12 @@ fnmatch('foo.txt', '*.txt')
 fnmatch('foo.txt', '?oo.txt')
 # True
 
-fnmatch('dat45.csv', 'Dat[0-9]*')
+fnmatch('Dat45.csv', 'Dat[0-9]*')
 # True
 
 names = ['Dat1.csv', 'Dat2.csv', 'config.ini', 'foo.py']
 
-# [name for name in names if fnmatch(name, 'Dat*.csv')]
+[name for name in names if fnmatch(name, 'Dat*.csv')]
 # ['Dat1.csv', 'Dat2.csv']
 
 
@@ -283,7 +283,7 @@ from fnmatch import fnmatchcase
 [addr for addr in addresses if fnmatchcase(addr, '* ST')]
 # ['5412 N CLARK ST', '1060 W ADDISON ST', '2122 N CLARK ST']
 
-# [addr for addr in addresses if fnmatchcase(addr, '54[0-9][0-9] *CLARK*')]
+[addr for addr in addresses if fnmatchcase(addr, '54[0-9][0-9] *CLARK*')]
 
 
 # The matching performed by fnmatch sits somewhere between the
@@ -294,3 +294,228 @@ from fnmatch import fnmatchcase
 
 # If you're actually trying to write code that matches filenames, use the
 # glob module instead.
+
+
+# 4) Matching and Searching for Text Patterns
+
+
+# You want to match or search text for a specific pattern.
+
+# If the text you're trying to match is a simple literal, you can often
+# just use the basic string methods, such as str.find(), str.endswith(),
+# str.startswith(), or similar.
+
+
+# For example:
+
+
+text = 'yeah, but no, but yeah, but no, but yeah'
+
+# Exact match
+
+text == 'yeah'
+# False
+
+# Match at start or end
+
+text.startswith('yeah')
+# True
+
+text.endswith('no')
+# False
+
+# Search for the location of the first occurrence
+
+text.find('no')
+# 10
+
+
+# For more complicated matching, use regular expressions and the re module.
+# To illustrate the basic mechanics of using regular expressions, suppose
+# you want to match dates specified as digits, such as "11/27/2012." Here
+# is a sample of how you would do it:
+
+
+text1 = '11/27/2012'
+text2 = 'Nov 27, 2012'
+
+import re
+# Simple matching: \d+ means match one or more digits
+
+if re.match(r'\d+/\d+/\d+', text1):
+    print('yes')
+else:
+    print('no')
+
+# yes
+
+if re.match(r'\d+/\d+/\d+', text2)
+    print('yes')
+else:
+    print('no')
+
+# no
+
+
+# If you're going to perform a lot of matches using the same pattern, it
+# usually pays to precompile the regular expression pattern into a pattern
+# object first.
+
+
+# For example:
+
+
+datepat = re.compile(r'\d+/\d+/\d+')
+if datepat.match(text1):
+    print('yes')
+else:
+    print('no')
+
+# yes
+
+if datepat.match(text2)
+    print('yes')
+else:
+    print('no')
+
+# no
+
+
+# match() always tries to find the match at the start of a string. If you
+# want to search text for all occurrences of a pattern, use the findall()
+# method instead. For example:
+
+
+text = 'Today is 11/27/2012. PyCon starts 3/13/2013.'
+
+datepat.findall(text)
+# ['11/27/2012', '3/13/2013']
+
+
+# When defining regular expressions, it is common to introduce capture
+# groups by enclosing parts of the pattern in parentheses.
+
+
+# For example:
+
+
+datepat = re.compile(r'(\d+)/(\d+)/(\d+)')
+
+
+# Capture groups often simplify subsequent processing of the matched text
+# because the contents of each group can be extracted individually.
+
+
+# For example:
+
+
+m = datepat.match('11/27/2012')
+
+m
+# <_sre.SRE_Match object at 0x1005d2750>
+
+# Extract the contents of each group
+m.group(0)
+# '11/27/2012'
+
+m.group(1)
+# '11'
+
+m.group(2)
+# '27'
+
+m.group(3)
+# '2012'
+
+m.groups()
+# ('11', '27', '2012')
+
+month, day, year = m.groups()
+
+
+# Find all matches (notice splitting into tuples)
+text
+# 'Today is 11/27/2012. PyCon start 3/13/2013.'
+
+datepat.findall(text)
+[('11', '27', '2012'), ('3', '13', '2013')]
+
+for month, day, year in datepat.findall(text):
+    print('{}-{}-{}'.format(year, month, day))
+
+# 2012-11-27
+# 2013-3-13
+
+
+# The findall() method searches the text and finds all matches, returning
+# them as a list. If you want to find matches iteratively, use the
+# finditer() method instead.
+
+
+# For example:
+
+
+for m in datepat.finditer(text):
+    print(m.groups())
+
+# ('11', '27', '2012')
+# ('3', '13', '2013')
+
+
+# A basic tutorial on the theory of regular expressions is beyond the scope
+# of this book. However, these examples illustrate the absolute basics of
+# using the re module to match and search for text. The essential
+# functionality is first compiling a pattern using re.compile() and then
+# using methods such as match(), findall(), or finditer().
+
+# When specifying patterns, it is relatively common to use raw strings such
+# as r'(\d+)/(\d+)/(\d+)'. Such strings leave the backslash character
+# uninterpreted, which can be useful in the context of regular expressions.
+# Otherwise, you need to use double backslashes such as
+# '(\\d+)/(\\d+)/(\\d+)'.
+
+# Be aware that the match() method only checks the beginning of a string.
+# It's possible that it will match things you aren't expecting.
+
+
+# For example:
+
+
+m = datepat.match('11/27/2012abcdef')
+
+m
+# <_sre.SRE_Match object at 0x1005d27e8>
+
+m.group()
+'11/27/2012'
+
+
+# If you want an exact match, make sure the pattern includes the end-marker
+# ($), as in the following:
+
+
+datepat = re.compile(r'(\d+)/(\d+)/(\d+)$')
+datepat.match('11/27/2012abcdef')
+
+datepat.match('11/27/2012')
+# <_sre.SRE_Match object at 0x1005d2750>
+
+
+# Last, if you're just doing a simple text matching/searching operation,
+# you can often skip the compilation step and use module-level functions
+# in the re module instead.
+
+
+# For example:
+
+
+re.findall(r'(\d+)/(\d+)/(\d+)', text)
+[('11', '27', '2012'), ('3', '13', '2013')]
+
+
+# Be aware, though, that if you're going to perform a lot of matching or
+# searching, it usually pays to compile the pattern first and use it over
+# and over again. The module-level functions keep a cache of recently
+# compiled patterns, so there isn't a huge performance hit, but you'll
+# save a few lookups and extra processing by using your own compiled
+# pattern.
